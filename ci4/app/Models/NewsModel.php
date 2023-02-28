60cd9a003fe40caf480247bf3aad67319b8ca56f
vemt17 <vmtan@student.apc.edu.ph>
2023-02-28 14:38:48 +0800

    news

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -7,6 +7,8 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
+
+    protected $allowedFields = ['title', 'slug', 'body'];
     
     public function getNews($slug = false)
     {
